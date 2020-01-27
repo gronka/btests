30938614ce5ad697e39f08872e3f72f36426b0a6
Taylor Gronka <mr.gronka@gmail.com>
2020-01-27 11:33:08 -0500

    changing for different project

--- setup.py
+++ setup.py
@@ -5,24 +5,24 @@ with open("README.md", "r") as fh:
 
 entry_points = {
     'console_scripts': [
-        'ftests = ftests.__main__:main'
+        'btests = btests.__main__:main'
     ]
 }
 
 setuptools.setup(
-    name="ftests",
+    name="btests",
     version="0.0.1",
     author="Taylor Gronka",
     author_email="mr.gronka@gmail.com",
-    description="Functional tests for flick",
+    description="Tests for bapi",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/gronka/ftests",
+    url="https://bitbucket.org/gronka/btests",
     packages=setuptools.find_packages('src'),
     package_dir={'': 'src'},
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Undetermined",
         "Operating System :: OS Independent",
     ],
     install_requires=[
